f76a3565250fde3d67cc6715b9db59cb46371fe0
MateiS2002 <stanescumatei11@gmail.com>
2025-12-21 15:32:39 +0200

    Initial Spring Boot project setup

---
+++ matquiz-spring-boot-backend/src/main/java/ro/mateistanescu/matquizspringbootbackend/MatquizSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.mateistanescu.matquizspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatquizSpringBootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatquizSpringBootBackendApplication.class, args);
+    }
+
+}
---
+++ matquiz-spring-boot-backend/src/test/java/ro/mateistanescu/matquizspringbootbackend/MatquizSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mateistanescu.matquizspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatquizSpringBootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
